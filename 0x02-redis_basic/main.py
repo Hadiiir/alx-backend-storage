829af2c61f84537c7f4cb4ff86307eb42062733b
Hadiiir <hadeer.gamal3010@gmail.com>
2025-07-09 14:55:35 +0300

    redis

---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import redis
+from exercise import Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
